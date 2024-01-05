bdcb46ba32c13d0b8a8877717663d4f59d45ac87
Luwam Mogos <luwam.mogosw@gmail.com>
2024-01-04 21:53:14 -0700

    counter greater than 0

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const increase = () => {
+    setCounter((prevCount) => prevCount + 1);
+  };
+  const decrease = () => {
+    if (counter > 0) {
+      setCounter((prevCount) => prevCount - 1);
+    }
+  };
+  const reset = () => {
+    setCounter(0);
+  };
 
   return (
     <>
@@ -14,6 +25,18 @@ function App() {
         <span>{counter}</span>
         <button onClick={() => setCounter((count) => count - 1)}>-</button>
         <button onClick={() => setCounter(0)}>Reset</button>
+      </div>
+      <div>
+        <button className="button" onClick={increase}>
+          +
+        </button>
+        <span>{counter}</span>
+        <button className="button" onClick={decrease}>
+          -
+        </button>
+        <button className="reset" onClick={reset}>
+          reset
+        </button>
       </div>
     </>
   );
